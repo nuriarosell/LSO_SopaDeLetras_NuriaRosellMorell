9df118ed2cf1d8afd2ae8d842b3f80793d6f97ef
nuriarosell <nuriarosell1999@gmail.com>
2024-04-12 09:02:36 +0200

    Corregir bugs

--- sopaDeLetras/src/main/java/model/SopaDeLetras.java
+++ sopaDeLetras/src/main/java/model/SopaDeLetras.java
@@ -5,6 +5,8 @@ import Utils.Utils;
 public class SopaDeLetras {
 
     private static char[][] sopa;
+    private static int palabrasEncontradas = 0;
+
 
     public static char[][] getSopa() {
         return sopa;
@@ -12,6 +14,9 @@ public class SopaDeLetras {
 
     public static boolean[][] getDescubiertas() {
         return descubiertas;
+    }
+    public static int getPalabrasEncontradas() {
+        return palabrasEncontradas;
     }
 
     private static boolean[][] descubiertas;
@@ -27,17 +32,23 @@ public class SopaDeLetras {
         for (int i = 0; i < sopa.length; i++) {
             for (int j = 0; j < sopa[i].length; j++) {
                 if (sopa[i][j] == palabra.charAt(0)) {
-                    //Comprobar vertical
-                    int[][] coords = comprobarPalabra(palabra, i, j);
-                    if (coords != null) {
-                        marcarLetrasDescubiertas(coords);
-                        return true;
+                    // Comprobar vertical
+                    int[][] coordsVertical = comprobarPalabra(palabra, i, j, true);
+                    if (coordsVertical != null) {
+                        marcarLetrasDescubiertas(coordsVertical);
+                        palabrasEncontradas++;
+                        if (palabrasEncontradas == 5) {
+                            return true; // Se encontraron 5 palabras, terminar la búsqueda
+                        }
                     } else {
-                        //Comprobar hoirzontal
-                        coords = comprobarPalabra(palabra, i, j);
-                        if (coords != null) {
-                            marcarLetrasDescubiertas(coords);
-                            return true;
+                        // Comprobar horizontal si no se encontró en vertical
+                        int[][] coordsHorizontal = comprobarPalabra(palabra, i, j, false);
+                        if (coordsHorizontal != null) {
+                            marcarLetrasDescubiertas(coordsHorizontal);
+                            palabrasEncontradas++;
+                            if (palabrasEncontradas == 5) {
+                                return true; // Se encontraron 5 palabras, terminar la búsqueda
+                            }
                         }
                     }
                 }
@@ -47,8 +58,9 @@ public class SopaDeLetras {
     }
 
 
+
     public static void marcarLetrasDescubiertas(int[][] coords) {
-    for (int i = 0; i < coords.length; i++) {
+        for (int i = 0; i < coords.length; i++) {
             descubiertas[coords[i][0]][coords[i][1]] = true;
         }
     }
@@ -57,21 +69,28 @@ public class SopaDeLetras {
     public static int[][] comprobarPalabra(String palabra, int i, int j, boolean vertical) {
         int[][] coords = new int[palabra.length()][2];
         int k = 0;
-        while (k < palabra.length() ) {
-            if (i < sopa.length && j < sopa[i].length && sopa[i][j] == palabra.charAt(k)) {
+        int incrementoI = (vertical) ? 1 : 0;
+        int incrementoJ = (vertical) ? 0 : 1;
+
+        while (k < palabra.length() && i < sopa.length && j < sopa[i].length) {
+            if (sopa[i][j] == palabra.charAt(k)) {
                 coords[k][0] = i;
                 coords[k][1] = j;
-                if (vertical)
-                    i++;
-                else
-                    j++;
+                i += incrementoI;
+                j += incrementoJ;
                 k++;
             } else {
-                return null;
+                return null; // Si no coincide la letra, retornar null
             }
         }
-        return coords;
+
+        if (k == palabra.length()) {
+            return coords; // Si se encontró la palabra completa, retornar las coordenadas
+        } else {
+            return null; // Si no se encontró completa, retornar null
+        }
     }
+
     public static int[][] comprobarPalabra(String palabra, int i, int j) {
         return comprobarPalabra(palabra, i, j, true);
     }
